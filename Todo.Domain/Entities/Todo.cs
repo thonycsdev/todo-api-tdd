656b8e1f96cd452ac9de234b042f9ec012cbfc59
Anthony Coutinho <thonycout@gmail.com>
2023-11-20 14:22:07 -0300

    First Entities

---
+++ Todo.Domain/Entities/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Todo.Domain.Entities
+{
+    public class Todo
+    {
+        public string Name { get; set; }
+        public DateTime? DueDate { get; set; }
+        public bool IsDone { get; set; }
+        public User CreatedBy { get; set; }
+        public int CreatedByUserId { get; set; }
+    }
+}
